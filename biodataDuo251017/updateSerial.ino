6cac7fd8938753a625ecc6a1e393bdfed2992c2a
eringee <erin.marie.gee@gmail.com>
2017-10-25 23:38:52 -0400

    Added GNU GPL License Information

--- biodataDuo251017/updateSerial.ino
+++ biodataDuo251017/updateSerial.ino
@@ -1,3 +1,21 @@
+/******************************************************
+copyright Erin Gee 2017
+
+Authors Erin Gee // Martin Peach // Thomas Ouellet-Fredericks
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    For more details: <http://www.gnu.org/licenses/>.
+
+******************************************************/
+
 void updateSerial() {
   Serial.print(heartSensorFiltered);
   Serial.print("\t");
